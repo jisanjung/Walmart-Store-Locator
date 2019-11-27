81fc16b74ddaf777881525677c593dff62f15511
Jisan Jung <jjjung13@gmail.com>
2019-11-27 13:23:50 -0500

    fully responsive

--- script.js
+++ script.js
@@ -2,11 +2,23 @@
 
 // initialize map view when page loads
 function initMap() {
-    // sets the view to the center of the united states
-    var map = L.map('mapid').setView([39.8283, -98.5795], 2);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    var mobile = window.matchMedia("(max-width: 768px)");
+
+    if (mobile.matches) {
+        // sets the view to the center of the united states
+        //zoom level is 2 on mobile
+        var map = L.map('mapid').setView([39.8283, -98.5795], 2);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    } else {
+        // sets the view to the center of the united states
+         //zoom level is 3 on desktop
+        var map = L.map('mapid').setView([39.8283, -98.5795], 3);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    }
 }
 
 // global function calls
