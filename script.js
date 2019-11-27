0829f480e97ce377b5b56557dfc8d2c445a2b131
Jisan Jung <jjjung13@gmail.com>
2019-11-27 12:22:08 -0500

    improved mobile view

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 // initialize map view when page loads
 function initMap() {
+    // sets the view to the center of the united states
     var map = L.map('mapid').setView([39.8283, -98.5795], 2);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
